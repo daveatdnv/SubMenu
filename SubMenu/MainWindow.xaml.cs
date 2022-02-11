834d02a955793f27cf6c0922670cd4fda52d1d87
David Moran <david.moran@dnv.com>
2022-02-11 16:38:51 +0000

    Add project files.

---
+++ SubMenu/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ActiproSoftware.Windows.Themes;
+
+namespace SubMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ThemeManager.CurrentTheme = ThemeNames.MetroDark;
+            ThemeManager.AreNativeThemesEnabled = true;
+
+            InitializeComponent();
+
+            MyContextMenu.ItemsSource = new List<MenuItem>
+            {
+                new MenuItem
+                {
+                    Name = "Menu 1",
+                    Items = new ObservableCollection<MenuItem>
+                    {
+                        new MenuItem
+                        {
+                            Name = "Menu 1.1", Items = new ObservableCollection<MenuItem>
+                            {
+                                new MenuItem { Name = "SubMenu 1.1.1" },
+                                new MenuItem { Name = "SubMenu 1.1.2" }
+                            }
+                        },
+                        new MenuItem
+                        {
+                            Name = "Menu 1.2", Items = new ObservableCollection<MenuItem>
+                            {
+                                new MenuItem { Name = "SubMenu 1.2.1" },
+                                new MenuItem { Name = "SubMenu 1.2.2" },
+                                new MenuItem { Name = "SubMenu 1.2.3" },
+                                new MenuItem { Name = "SubMenu 1.2.4" }
+                            }
+                        }
+                    }
+                },
+            };
+        }
+    }
+}
---
+++ SubMenu/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SubMenu
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+
+        public ObservableCollection<MenuItem> Items { get; set; }
+    }
+
+}
